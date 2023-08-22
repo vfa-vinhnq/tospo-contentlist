62a0440ba0af4d3d05f03881d7db64b08d3139cd
vfa-vinhnq <vinhnq@vitalify.asia>
2023-08-22 11:15:45 +0700

    Update member content table and delete reporter table

--- assets/js/wlc.js
+++ assets/js/wlc.js
@@ -1,39 +1,42 @@
-window.onload = function(){
-	onReady();
+window.onload = function () {
+	onReady()
 }
-function onReady(){
-	setTimeout(function(){
-		$('body').removeClass('notReady');
-		$('body').addClass('ready');
-		if(page=="top"){
-			$("meta[name ='theme-color']").attr('content','#e51600');
+function onReady() {
+	setTimeout(function () {
+		$('body').removeClass('notReady')
+		$('body').addClass('ready')
+		if (page == 'top') {
+			$("meta[name ='theme-color']").attr('content', '#e51600')
 		}
-		$('.inviewTarget').on('inview', function(event, isInView){
+		$('.inviewTarget').on('inview', function (event, isInView) {
 			if (isInView) {
-				var t = this;
-				setTimeout(function(){
-					$(t).removeClass('inview');
-				},200);
-		}
-	  });
-	},1000);
+				var t = this
+				setTimeout(function () {
+					$(t).removeClass('inview')
+				}, 200)
+			}
+		})
+	}, 1000)
 
+	$('a[href^="#"]').click(function () {
+		var href = $(this).attr('href')
+		var target = $(href == '#' || href == '' ? 'html' : href)
+		var position = target.offset().top
+		var speed = 500
+		$('html, body').animate(
+			{
+				scrollTop: position,
+			},
+			speed,
+			'swing'
+		)
+		return false
+	})
+}
+function openPP() {
+	$('#pp').css('display', 'block')
+}
+function closePP() {
+	$('#pp').css('display', 'none')
+}
 
-  $('a[href^="#"]').click(function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    var speed = 500;
-    $("html, body").animate({
-      scrollTop: position
-    }, speed, "swing");
-    return false;
-  });
-
-}
-function openPP(){
-	$("#pp").css("display","block");
-}
-function closePP(){
-	$("#pp").css("display","none");
-}
